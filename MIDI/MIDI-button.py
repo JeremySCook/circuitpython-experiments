740c8e1e71b765b858bf56e8e3d8707539de7b88
Jeremy Cook <jscook55@gmail.com>
2022-12-27 15:23:56 -0500

    Update MIDI-button.py

--- MIDI/MIDI-button.py
+++ MIDI/MIDI-button.py
@@ -1,5 +1,6 @@
-# Modification of example code found here: https://docs.circuitpython.org/projects/midi/en/latest/
-# to add a button
+# modification of code: https://docs.circuitpython.org/projects/midi/en/latest/
+# Adds a button on pin 16
+# @JeremySCook 12/27/2022
 
 import time
 import random
@@ -18,14 +19,22 @@ button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 
 midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
+key_on = 0
 
 print("Midi test")
 
 print("Default output MIDI channel:", midi.out_channel + 1)
 
+midi.send(ControlChange(64, 0))  # sustain CC - e.g. 0 = off, 127 = on
+
 while True:
-    if button.value:
-        midi.send(NoteOff(44, 120))   
-    else:
-        midi.send(NoteOn(44, 120))  # G sharp 2nd octave
-        time.sleep(0.1)
+    if button.value == 0 and key_on == 0:
+        midi.send(NoteOn(44, 120))  # G sharp 2nd octave, 44th key on piano - velocity = 120
+        key_on = 1
+        time.sleep(.05) # debounce
+        # midi.send(NoteOff(44, 120))
+    elif button.value == 1 and key_on == 1:
+        midi.send(NoteOff(44, 120))
+        key_on = 0
+        time.sleep(.05) # debounce
+
