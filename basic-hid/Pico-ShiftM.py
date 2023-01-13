e3bb93ff9562a167b66efc021299daa9e1f4fd9c
Jeremy Cook <jscook55@gmail.com>
2023-01-13 12:01:47 -0500

    Create Pico-ShiftM.py

---
+++ basic-hid/Pico-ShiftM.py
@@ -0,0 +1,21 @@
+import time
+import digitalio
+import board
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+
+btn1_pin = board.GP16
+
+keyboard = Keyboard(usb_hid.devices)
+
+btn1 = digitalio.DigitalInOut(btn1_pin)
+btn1.direction = digitalio.Direction.INPUT
+btn1.pull = digitalio.Pull.UP
+
+while True:
+    if not btn1.value:
+        print("button 1 pressed")
+        keyboard.press(Keycode.LEFT_SHIFT, Keycode.M)
+        time.sleep(.2)
+        keyboard.release(Keycode.LEFT_SHIFT, Keycode.M)
