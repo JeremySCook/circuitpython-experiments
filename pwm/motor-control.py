cbb8d3c5751fea6106795bd268ddb10edfcd31ae
Jeremy Cook <jscook55@gmail.com>
2023-02-22 14:16:46 -0500

    Update motor-control.py

--- pwm/motor-control.py
+++ pwm/motor-control.py
@@ -11,8 +11,9 @@ led = pwmio.PWMOut(board.GP16, frequency=5000, duty_cycle=0)
 
 while True:
     led.duty_cycle = (2 ** 16 -1) # 65535 - full scope of PWM
-    time.sleep(2)
+    time.sleep(5)
     led.duty_cycle = (2 ** 15 -1) # 32767
-    time.sleep(2)
+    time.sleep(5)
     led.duty_cycle = 0
-    time.sleep(2)
+    time.sleep(5)
+  
