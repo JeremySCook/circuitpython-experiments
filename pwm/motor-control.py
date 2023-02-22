59953328d8c91583a109ed09da9767004a473727
Jeremy Cook <jscook55@gmail.com>
2023-02-22 14:22:12 -0500

    Update motor-control.py

--- pwm/motor-control.py
+++ pwm/motor-control.py
@@ -1,6 +1,7 @@
 # PWM example for RPi Pico
 # Used for motor - works w/ 12V input at different speeds
 # Simply cuts off at 5V when not at max PWM, though decelerates more slowly than if entirely cut off
+# Does not start at 50%, but can maintain rotation at that input
 
 import time
 import pwmio
@@ -10,10 +11,11 @@ import digitalio
 led = pwmio.PWMOut(board.GP16, frequency=5000, duty_cycle=0)
 
 while True:
+    led.duty_cycle = (2 ** 15 -1) # 32767
+    time.sleep(5)
     led.duty_cycle = (2 ** 16 -1) # 65535 - full scope of PWM
     time.sleep(5)
     led.duty_cycle = (2 ** 15 -1) # 32767
     time.sleep(5)
     led.duty_cycle = 0
     time.sleep(5)
-  
