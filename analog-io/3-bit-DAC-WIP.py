615cfeed4f002a7dca5affbf6df09c2f49abeb2d
Jeremy Cook <jscook55@gmail.com>
2023-04-12 16:47:23 -0400

    Update 3-bit-DAC-WIP.py

--- analog-io/3-bit-DAC-WIP.py
+++ analog-io/3-bit-DAC-WIP.py
@@ -1,13 +1,11 @@
-#Attempt to clean up, not yet working
-
 from time import sleep
-from adafruit_itertools import cycle
+from itertools import cycle
 
 # other parts unchanged, though it could still be cleaned up, too.
 
 #identical in behavior to the while(True) loop
 for x in cycle(range(8)):
-   digits = [int(y) for y in bin(x)[2:]]
+   digits = [int(y) for y in (3-len(bin(x)[2:]) * '0' + bin(x)[2:]]
    ASB.value, BSB.value, CSB.value = digits[-3:]
    print("binary {}".format(x))
    sleep(SLEEP_DELAY)
