e86fd626e8ff26211ed0bac05615473d3c32e22e
Jeremy Cook <jscook55@gmail.com>
2023-05-03 15:52:22 -0400

    Create tonepwmio.py

---
+++ sound-playback/PWMtone/tonepwmio.py
@@ -0,0 +1,12 @@
+import time
+import pwmio
+import board
+import digitalio
+
+speaker = pwmio.PWMOut(board.GP26, frequency=440, duty_cycle=32768, variable_frequency = 1) # initializer creates instance of pwmio class
+
+while True:
+    speaker.frequency = 440
+    time.sleep(1)
+    speaker.frequency = 800
+    time.sleep(1)
