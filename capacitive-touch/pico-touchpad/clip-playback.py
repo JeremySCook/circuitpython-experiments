4a2e9802969aeb0972d3f486ff9cdbe9f9a4c1d0
Jeremy Cook <jscook55@gmail.com>
2023-05-23 17:57:36 -0400

    Update clip-playback.py

--- capacitive-touch/pico-touchpad/clip-playback.py
+++ capacitive-touch/pico-touchpad/clip-playback.py
@@ -1,65 +1,94 @@
+# SPDX-FileCopyrightText: 2022 John Park for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# Breakbeat Breadboard based on:
+# @todbot / Tod Kurt - https://github.com/todbot/plinkykeeb
+# Convert files to appropriate WAV format (mono, 22050 Hz, 16-bit signed) with command:
+#  sox loop.mp3 -b 16 -c 1 -r 22050 loop.wav
+# put samples in "/wav" folder
+
 # SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
 
 """
-CircuitPython single MP3 playback example for Raspberry Pi Pico.
-Plays a single MP3 once.
--Modified JSCook 5/11/2023 from example found here: https://learn.adafruit.com/mp3-playback-rp2040/pico-mp3
--JSCook 5/22/2023 WIP for multiple clip playback based on touchpad - still need a filter to prevent pops? Can be fixed programatically?
+Modified - Still WIP JSCook 5/23/2023
 """
+
+import time
 import board
-import audiomp3
-import audiopwmio
 import digitalio
 import touchio
-import time
+import audiocore
+import audiomixer
+from audiopwmio import PWMAudioOut as AudioOut
+from adafruit_debouncer import Debouncer, Button
 
-audio = audiopwmio.PWMAudioOut(board.GP26)
+# wait a little bit so USB can stabilize and not glitch audio
+time.sleep(1)
+print("Hello")
+
+# list of (samples to play, mixer gain level)
+wav_files = (
+    ('wav/amen_22k16b_160bpm.wav', 1.0),
+    ('wav/pt_limor_modem_vox_01.wav', 0.4),
+    ('wav/snowpeaks_22k_s16.wav', 0.8)
+)
+
+touch_pins = (
+    board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5,
+    board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11,
+    board.GP12, board.GP13, board.GP14, board.GP15
+)
+touch_pads = []
+touch_ins = []
+
+for pin in touch_pins:
+    touchin = touchio.TouchIn(pin)
+    touchin.threshold = 1500
+    touch_pads.append( Button(touchin, value_when_pressed=True))
+    touch_ins.append(touchin)  # for debug
+
+num_touch_pads = len(touch_pads)
+
+
+audio = AudioOut(board.GP26)
 
 speaker_stereo = digitalio.DigitalInOut(board.GP27)
 speaker_stereo.direction = digitalio.Direction.INPUT # set to tri-state if stereo not use
 
-touch_pin0 = touchio.TouchIn(board.GP0)
-touch_pin1 = touchio.TouchIn(board.GP1)
-touch_pin2 = touchio.TouchIn(board.GP2)
-touch_pin3 = touchio.TouchIn(board.GP3)
-touch_pin4 = touchio.TouchIn(board.GP4)
-touch_pin5 = touchio.TouchIn(board.GP5)
-touch_pin6 = touchio.TouchIn(board.GP6)
-touch_pin7 = touchio.TouchIn(board.GP7)
-touch_pin8 = touchio.TouchIn(board.GP8)
-touch_pin9 = touchio.TouchIn(board.GP9)
-touch_pin10 = touchio.TouchIn(board.GP10)
-touch_pin11 = touchio.TouchIn(board.GP11)
-touch_pin12 = touchio.TouchIn(board.GP12)
-touch_pin13 = touchio.TouchIn(board.GP13)
-touch_pin14 = touchio.TouchIn(board.GP14)
-touch_pin15 = touchio.TouchIn(board.GP15)
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
 
-touch_pin0.threshold = 1500
-touch_pin1.threshold = 1500
-touch_pin2.threshold = 1500
-touch_pin3.threshold = 1500
-touch_pin4.threshold = 1500
-touch_pin5.threshold = 1500
-touch_pin6.threshold = 1500
-touch_pin7.threshold = 1500
-touch_pin8.threshold = 1500
-touch_pin9.threshold = 1500
-touch_pin10.threshold = 1500
-touch_pin11.threshold = 1500
-touch_pin12.threshold = 1500
-touch_pin13.threshold = 1500
-touch_pin14.threshold = 1500
-touch_pin15.threshold = 1500
+mixer = audiomixer.Mixer(voice_count=len(wav_files), sample_rate=22050, channel_count=1,
+                         bits_per_sample=16, samples_signed=True)
+audio.play(mixer) # attach mixer to audio playback
 
-decoder = audiomp3.MP3Decoder(open("bariuke-22050-32k.mp3", "rb"))
+for i in range(len(wav_files)):  # start all samples at once for use w handle_mixer
+    wave = audiocore.WaveFile(open(wav_files[i][0],"rb"))
+    mixer.voice[i].play(wave, loop=True)
+    mixer.voice[i].level = 0
+
+def handle_mixer(num, pressed):
+    voice = mixer.voice[num]   # get mixer voice
+    if pressed:
+        wave = audiocore.WaveFile(open(wav_files[i][0],"rb"))
+        voice.play(wave, loop=False)
+        voice.level = 1 # play at level in wav_file list
+        print("playing clip ", num)
+    else: # released
+        voice.level = 0  # mute it
+
 
 while True:
-    if touch_pin0.value:
-        audio.play(decoder)
-        while audio.playing:
-            pass
-
-print("Done playing!")
+    for i in range(len(wav_files)):
+        touch = touch_pads[i]
+        touch.update()  
+        if touch.rose:
+            led.value = True
+            handle_mixer(i, True)
+            print(i, " True")
+        if touch.fell:
+            led.value = False
+            handle_mixer(i, False)
+            print(i, " Fale")
