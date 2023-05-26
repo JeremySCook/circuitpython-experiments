ce44aee08903cf08e354f7c514fbd3a655d6fa0e
Jeremy Cook <jscook55@gmail.com>
2023-05-26 13:43:29 -0400

    Update tonepwmio.py

--- sound-playback/PWMtone/tonepwmio.py
+++ sound-playback/PWMtone/tonepwmio.py
@@ -2,7 +2,11 @@ import time
 import pwmio
 import board
 
-speaker = pwmio.PWMOut(board.GP26, frequency=440, duty_cycle=32768, variable_frequency = 1) # initializer creates instance of pwmio class
+speaker = pwmio.PWMOut(board.GP26, frequency=440, duty_cycle=32768, variable_frequency = 1) 
+# initializer creates instance of pwmio class
+# modify speaker.duty_cycle to change perceived volume
+# duty cycle full PWM range is 0 to 65535. 65535 which would mean on all the time
+# and not really oscillating
 
 while True:
     speaker.frequency = 440
