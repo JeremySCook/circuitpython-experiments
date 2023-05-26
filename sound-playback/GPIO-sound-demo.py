a7f354060b03ff93c0ecdbed5ec24b0e59aee040
Jeremy Cook <jscook55@gmail.com>
2023-05-26 14:04:25 -0400

    Update GPIO-sound-demo.py

--- sound-playback/GPIO-sound-demo.py
+++ sound-playback/GPIO-sound-demo.py
@@ -5,7 +5,7 @@
 """
 CircuitPython single MP3 playback example for Raspberry Pi Pico.
 Plays a single MP3 once.
--Modified JSCook 5/11/2023 from example found here: https://learn.adafruit.com/mp3-playback-rp2040/pico-mp3
+-Modified JSCook 5/11/2023
 """
 import board
 import audiomp3
@@ -13,7 +13,7 @@ import audiopwmio
 
 audio = audiopwmio.PWMAudioOut(board.GP26)
 
-decoder = audiomp3.MP3Decoder(open("bariuke-22050-32k.mp3", "rb"))
+decoder = audiomp3.MP3Decoder(open("bariuke-22050.mp3", "rb"))
 
 while True:
     audio.play(decoder)
