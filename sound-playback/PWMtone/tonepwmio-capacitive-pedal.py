54e47c670c8ef13f150e6f393cc8ea4ef21e96cb
Jeremy Cook <jscook55@gmail.com>
2023-05-26 13:59:20 -0400

    Update tonepwmio-capacitive-pedal.py

--- sound-playback/PWMtone/tonepwmio-capacitive-pedal.py
+++ sound-playback/PWMtone/tonepwmio-capacitive-pedal.py
@@ -12,20 +12,20 @@ touch_pin0.threshold = 3000
 touch_pin1.threshold = 3000
 touch_pin2.threshold = 3000
 
-led = digitalio.DigitalInOut(board.LED) # defaults to input
+led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-speaker = pwmio.PWMOut(board.GP26, frequency=440, duty_cycle=0, variable_frequency = 1) # initializer creates instance of pwmio class
+speaker = pwmio.PWMOut(board.GP26, frequency=440, duty_cycle=0, variable_frequency = 1)
 
 while True:
     if touch_pin0.value:
-        speaker.frequency = 65 # C2 ~65.41Hz
+        speaker.frequency = 65
     if touch_pin1.value:
-        speaker.frequency = 69 # C2# ~69.3Hz
+        speaker.frequency = 69
     if touch_pin2.value:
-        speaker.frequency = 73 # D ~73.42Hz
-        
-    time.sleep(.02) # 10ms sleep time
+        speaker.frequency = 73
+    
+    time.sleep(.02)
     
     if touch_pin0.value or touch_pin1.value or touch_pin2.value:
         led.value = True
@@ -33,4 +33,6 @@ while True:
     else:
         led.value = False
         speaker.duty_cycle = 0
-    print("GP0 ", touch_pin0.value, touch_pin0.raw_value, touch_pin0.threshold, "  GP1 ", touch_pin1.value, "  GP2 ", touch_pin2.value)
+        
+        print("GP0 ", touch_pin0.value, touch_pin0.raw_value, touch_pin0.threshold, \
+        "  GP1 ", touch_pin1.value, "  GP2 ", touch_pin2.value)
