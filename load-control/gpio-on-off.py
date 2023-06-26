16521cf1fa57c6aef47412e7126052c09472aa5e
Jeremy Cook <jscook55@gmail.com>
2023-06-26 09:59:22 -0400

    Update gpio-on-off.py

--- load-control/gpio-on-off.py
+++ load-control/gpio-on-off.py
@@ -4,6 +4,9 @@ import time
 
 step_delay = .1
 
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
 output_right = digitalio.DigitalInOut(board.GP16)
 output_right.direction = digitalio.Direction.OUTPUT
 
@@ -11,7 +14,12 @@ output_left = digitalio.DigitalInOut(board.GP15)
 output_left.direction = digitalio.Direction.OUTPUT
 
 while True:
+    output_right.value = 1
     output_left.value = 1
+    led.value = 1
     time.sleep(1)
+    
+    output_right.value = 0
     output_left.value = 0
+    led.value = 0
     time.sleep(3)
