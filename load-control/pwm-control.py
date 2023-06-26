73b6f56db1d1101611cd5e910188a9111402af47
Jeremy Cook <jscook55@gmail.com>
2023-06-26 16:07:33 -0400

    Update pwm-control.py

--- load-control/pwm-control.py
+++ load-control/pwm-control.py
@@ -2,16 +2,12 @@ import time
 import pwmio
 import board
 
-speaker = pwmio.PWMOut(board.GP15, frequency=440, duty_cycle=32768, variable_frequency = 0) 
-# initializer creates instance of pwmio class
-# modify speaker.duty_cycle to change perceived volume
-# duty cycle full PWM range is 0 to 65535. 65535 which would mean on all the time
-# and not really oscillating
+ezfan = pwmio.PWMOut(board.GP15, frequency=440, duty_cycle=0, variable_frequency=0)
 
 while True:
-    speaker.duty_cycle = 65535
-    print(speaker.duty_cycle)
+    ezfan.duty_cycle = 65535
+    print(ezfan.duty_cycle)
     time.sleep(1)
-    speaker.duty_cycle = int(65535/3)
-    print(speaker.duty_cycle)
+    ezfan.duty_cycle = int(65535/3)
+    print(ezfan.duty_cycle)
     time.sleep(1)
