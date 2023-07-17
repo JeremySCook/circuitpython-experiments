92f10f5d903a0ea4f6402a47d300f1a884a502e4
Jeremy Cook <jscook55@gmail.com>
2023-07-17 09:03:24 -0400

    Update analog-touch.py

--- capacitive-touch/analog-touch.py
+++ capacitive-touch/analog-touch.py
@@ -2,12 +2,10 @@ import board
 import touchio
 import time
 
-touch_pin18 = touchio.TouchIn(board.GP18)
-touch_pin18.threshold = 3000 # 3000 is inconsistent with the smaller wire
-touch_pin19 = touchio.TouchIn(board.GP19)
-touch_pin19.threshold = 3000
+touch_pinA = touchio.TouchIn(board.GP14)
+touch_pinB = touchio.TouchIn(board.GP15)
 
 while True:
-    print((touch_pin18.raw_value,touch_pin19.raw_value))
-    print ("GP18 ", touch_pin18.value,"  GP19 ", touch_pin19.value)
+    print((touch_pinA.raw_value, touch_pinB.raw_value, \
+    (touch_pinA.raw_value - touch_pinB.raw_value)))
     time.sleep(.1)
