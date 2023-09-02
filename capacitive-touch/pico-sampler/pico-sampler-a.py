0daca86599224b910ba8e9cd7f536095756af249
Jeremy Cook <jscook55@gmail.com>
2023-09-01 21:39:21 -0400

    Create pico-sampler-a.py

---
+++ capacitive-touch/pico-sampler/pico-sampler-a.py
@@ -0,0 +1,28 @@
+import board
+import touchio
+import time
+import digitalio
+
+touch_pinA = touchio.TouchIn(board.PAD1)
+touch_pinB = touchio.TouchIn(board.PAD2)
+
+led0_pin = board.GP23
+led0_out = digitalio.DigitalInOut(led0_pin)
+led0_out.direction = digitalio.Direction.OUTPUT
+
+led1_pin = board.GP24
+led1_out = digitalio.DigitalInOut(led1_pin)
+led1_out.direction = digitalio.Direction.OUTPUT
+
+while True:
+    print("GP18 ", touch_pinA.value, " GP19 ", touch_pinB.value)
+    time.sleep(.1)
+    if touch_pinA.value:
+        led1_out.value = 1
+    else:
+        led1_out.value = 0
+        
+    if touch_pinB.value:
+        led0_out.value = 1
+    else:
+        led0_out.value = 0
